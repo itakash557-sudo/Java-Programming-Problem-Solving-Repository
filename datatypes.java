5f634232eaf8287fb1fa73124f52c2e244cec0f7
itakash557@gmail.com <aswiniakash2006@gmail.com>
2026-04-06 19:57:39 +0530

    datatypes

---
+++ datatypes.java
@@ -0,0 +1,23 @@
+class datatypes{
+    public static void main(String[]args){
+        System.out.println("Data types in java");
+        byte a=10;
+        short b=30000;;
+        int c=123456;
+        long d=123546678978875l;
+        float e=3.56489756f;
+        double f=1.23559797777777979;
+        char g='a';
+        boolean h= true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+    }
+}
