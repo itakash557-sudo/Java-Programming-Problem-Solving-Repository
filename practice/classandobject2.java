93d440667bd12b95b52863f51dc242aad627db23
itakash557@gmail.com <aswiniakash2006@gmail.com>
2026-06-19 06:50:10 +0530

    adding program

---
+++ practice/classandobject2.java
@@ -0,0 +1,16 @@
+class classandobject2
+{
+    String name;
+    int age;
+    void fn()
+    {
+        System.out.println(name);
+        System.out.println(age);
+    }
+    public static void main(String[]args){
+    classandobject2 obj=new classandobject2();
+    obj.name="java";
+    obj.age=10;
+    obj.fn();
+}
+}
