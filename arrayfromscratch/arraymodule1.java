7c8c79ea0d381a314892dd73411d8710a573dfd6
itakash557@gmail.com <aswiniakash2006@gmail.com>
2026-07-12 08:27:17 +0530

    adding program

---
+++ arrayfromscratch/arraymodule1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class arraymodule1
+{
+    public static void main(String[]args)
+    {
+        int [] arr={10,20,30,40,50};
+        String [] str={"akash","developer","java"};
+        char [] ch={'a','b','c','d','e'};
+        System.out.println(Arrays.toString(arr));        
+        System.out.println(Arrays.toString(str));
+        System.out.println(Arrays.toString(ch));
+    }
+}
