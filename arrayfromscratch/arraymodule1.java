27078fad24d15ad191a4dab2c608bfbde5fc874a
itakash557@gmail.com <aswiniakash2006@gmail.com>
2026-07-27 06:10:50 +0530

    adding programs

--- arrayfromscratch/arraymodule1.java
+++ arrayfromscratch/arraymodule1.java
@@ -3,11 +3,28 @@ class arraymodule1
 {
     public static void main(String[]args)
     {
-        int [] arr={10,20,30,40,50};
-        String [] str={"akash","developer","java"};
-        char [] ch={'a','b','c','d','e'};
+        int [] arr={10,20,30,40,50}; //java creates a object automatically using new keyword
+        int [] arrr=new int[2]; //created by using new keyword
+        arrr[0]=10;
+        arrr[1]=20;
+        String [] str;
+        str=new String[]{"akash","developer","java"};
+        char [] ch;
+        ch=new char[2];
+        ch[0]='A';
+        ch[1]='a';
+
+        System.out.println(Arrays.toString(arrr));
         System.out.println(Arrays.toString(arr));        
         System.out.println(Arrays.toString(str));
         System.out.println(Arrays.toString(ch));
+        System.out.println(str.length);
+
+
+        int[] array1={10,20,30,60,40,50};
+        for(int i=0;i<array1.length;i++){
+            System.out.println(array1[i]);
+        }
+        
     }
 }
