ef6930e728b1d7ad2cc983c29fae77d3c39b05aa
itakash557@gmail.com <aswiniakash2006@gmail.com>
2026-07-31 06:21:50 +0530

    adding program

--- arrayfromscratch/arraymodule1.java
+++ arrayfromscratch/arraymodule1.java
@@ -21,10 +21,17 @@ class arraymodule1
         System.out.println(str.length);
 
 
-        int[] array1={10,20,30,60,40,50};
-        for(int i=0;i<array1.length;i++){
+        int[] array1={10,20,30,60,40,50,60,70};
+        for(int i=0;i<array1.length;i++){ //i=5 length=6  5<6                       
             System.out.println(array1[i]);
         }
         
+        //deletion element in Array 
+
+        int [] array2={10,20,30,40,50};
+        for(int i=1;i<array2.length-1;i++){
+            arr[i]=arr[i+1];
+        }
+        System.out.println(Arrays.toString(array2));
     }
 }
