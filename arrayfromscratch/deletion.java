2280372b441b5d77bea2f5866a27aa5d60a7d727
itakash557@gmail.com <aswiniakash2006@gmail.com>
2026-08-07 04:10:02 +0530

    adding program

--- arrayfromscratch/deletion.java
+++ arrayfromscratch/deletion.java
@@ -2,9 +2,28 @@ import java.util.Arrays;
 class deletion{
     public static void main(String[]args)
     {
-        System.out.println("deletion in Array");
-        int[]arr={10,20,25,30,40,50};
-        System.out.println(arr(indexAt(25)));
+        System.out.println("Deletion in array");
+        int[] array={10,20,25,30,40};
+        for(int i =2;i<array.length-1;i++){
+            array[i]=array[i+1];
+        }
+    System.out.println(Arrays.toString(array));
 
+
+
+
+        System.out.println("method 2"); //finding the number in array and deletion the number 
+        int[]array2={10,20,30,40,45,50,60,70,80,90};
+        for(int i=0;i<array2.length-1;i++)
+        {
+            if(array2[i]==45)
+            {
+                for(int j=i;j<array2.length-1;j++){
+                array2[j]=array2[j+1];
+                }
+                System.out.print(Arrays.toString(array2));
+            }
+
+        }
     }
 }
